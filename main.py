e5ad208ea0660e8766b6db2125d37138f7cfa143
Flipp3rrr <flipp3rrr@protonmail.com>
2021-09-24 16:37:34 +0200

    the picture command now works!!!!

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import json
 import discord
+from discord.ext import commands
+import random
 
 # Settings are saved in 'settings.json', settings are saved in a dictionary. This function gets the value of a key specified ('name').
 def get_setting(name):
@@ -36,13 +38,42 @@ def get_setting(name):
         return(return_value)
 
 token = get_setting("token")
+run_dir = os.path.dirname(__file__)
+picture_dir = os.path.join(run_dir, "pictures")
 
-class client(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+intents = discord.Intents.default()
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+bot = commands.Bot(command_prefix=";;", intents=intents)
 
-client = client()
-client.run(token)
+# Thanks to StackOverflow 'https://stackoverflow.com/questions/7099290/how-to-ignore-hidden-files-using-os-listdir'
+def listdir_nohidden(path):
+    for f in os.listdir(path):
+        if not f.startswith('.'):
+            yield f
+
+@bot.event
+async def on_ready():
+    print("Logged in as {user} ({id})".format(user = bot.user.name, id = bot.user.id))
+
+@bot.command()
+async def list_collections(ctx, description = "List all available collections"):
+    collections = listdir_nohidden(picture_dir)
+    await ctx.send("Possible collections: {list}".format(list = collections))
+
+@bot.command()
+async def picture(ctx, collection:str, description = "Get a random image from a collection specified"):
+    collection_dir = os.path.join(picture_dir, collection)
+
+    author_choices = list(listdir_nohidden(collection_dir))
+    author_choice_index = random.randrange(len(author_choices))
+    author_chosen = author_choices[author_choice_index]
+    author_dir = os.path.join(collection_dir, author_chosen)
+
+    image_choices = list(listdir_nohidden(author_dir))
+    image_choice_index = random.randrange(len(image_choices))
+    image_chosen = image_choices[image_choice_index]
+    image_path = os.path.join(author_dir, image_chosen)
+
+    await ctx.send("Here's an image from the '{collection}' collection, submitted by {author}".format(collection = collection, author = author_chosen), file=discord.File(image_path))
+
+bot.run(token)
