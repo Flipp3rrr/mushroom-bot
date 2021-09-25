90ddf261f93a0c4bfe8e792c52fcd17f22dacba4
Flipp3rrr <flipp3rrr@protonmail.com>
2021-09-25 23:45:13 +0200

    the random image is now selected in a way i like more and the prefix is customisable

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import discord
 from discord.ext import commands
 import random
+import pathlib
 
 run_dir = os.path.dirname(__file__)
 
@@ -47,7 +48,7 @@ picture_dir = os.path.join(run_dir, "pictures")
 
 intents = discord.Intents.default()
 
-bot = commands.Bot(command_prefix=";;", intents=intents)
+bot = commands.Bot(command_prefix=get_setting("prefix"), intents=intents)
 
 # Thanks to StackOverflow 'https://stackoverflow.com/questions/7099290/how-to-ignore-hidden-files-using-os-listdir'
 def listdir_nohidden(path):
@@ -66,19 +67,14 @@ async def list_collections(ctx, description = "List all available collections"):
 
 @bot.command()
 async def picture(ctx, collection:str, description = "Get a random image from a collection specified"):
-    collection_dir = os.path.join(picture_dir, collection)
+    collection_dir = pathlib.Path(picture_dir) / collection
 
-    author_choices = list(listdir_nohidden(collection_dir))
-    author_choice_index = random.randrange(len(author_choices))
-    author_chosen = author_choices[author_choice_index]
-    author_dir = os.path.join(collection_dir, author_chosen)
+    jpegs = list(collection_dir.glob("**/*.jpg"))
+    choice = random.choice(jpegs)
+    latest_parent = choice.parent.name
+    print("Chose {file} from {author}".format(file = choice, author = latest_parent))
 
-    image_choices = list(listdir_nohidden(author_dir))
-    image_choice_index = random.randrange(len(image_choices))
-    image_chosen = image_choices[image_choice_index]
-    image_path = os.path.join(author_dir, image_chosen)
-
-    await ctx.send("Here's an image from the '{collection}' collection, submitted by {author}".format(collection = collection, author = author_chosen), file=discord.File(image_path))
+    await ctx.send("Here's an image from the '{collection}' collection, submitted by {author}".format(collection = collection, author = latest_parent), file=discord.File(choice))
 
 @bot.command()
 async def info(ctx, description = "Get information about the bot"):
