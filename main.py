Flipp3rrr/mushroom-bot

commit 34cd376fb3d5dbb7e35ae043d5e475a8e22621b5

bot now won't reply to itself

main.py
@@ -68,6 +68,8 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    if message.author == client.user:
+        return
     if message.content.startswith("pretty"):
         # Reset the collections_list, otherwise it would accumulate empty items over time
         collections_list = list(listdir_nohidden(picture_dir))

commit 08d7a88c06b2d7207650ce3d4b5a4bbf9a6a4f75

fix bug introduced in commit 34cd376

main.py
@@ -68,8 +68,9 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
+
     if message.content.startswith("pretty"):
         # Reset the collections_list, otherwise it would accumulate empty items over time
         collections_list = list(listdir_nohidden(picture_dir))