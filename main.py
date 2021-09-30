46d5434b0c4daf7f55034e8377fee04453ff3fc9
Flipp3rrr <flipp3rrr@protonmail.com>
2021-09-30 09:01:11 +0200

    change 'requested by' -> 'submitted by', use 'id' so it changes when someone changes their username

--- main.py
+++ main.py
@@ -57,7 +57,11 @@ bot_invite = "https://discord.com/api/oauth2/authorize?client_id=890578768849158
 github_link = "https://github.com/Flipp3rrr/mushroom-bot"
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix=bot_prefix, intents=intents)
+intents.members = True
+
+member_cache_flags = discord.MemberCacheFlags.from_intents(intents)
+
+bot = commands.Bot(command_prefix = bot_prefix, intents = intents, member_cache_flags = member_cache_flags)
 bot.remove_command("help")
 
 @bot.event
@@ -86,11 +90,14 @@ async def on_message(message):
                 
                 jpegs = list(collection_dir.glob("**/*.jpg"))
                 choice = random.choice(jpegs)
+                author = bot.get_user(choice.parent.name)
+                if author is None:
+                    author = await bot.fetch_user(choice.parent.name)
 
                 embed = discord.Embed(title = "{collection} picture".format(collection = collection))
                 image = discord.File(choice, filename = choice.name)
                 embed.set_image(url = "attachment://{file}".format(file = choice.name))
-                embed.set_footer(text = "Requested by {message_author}".format(message_author = message.author))
+                embed.set_footer(text = "Image submitted by {author}".format(author = author))
                 await message.channel.send(file = image, embed = embed)
     
     await bot.process_commands(message)
@@ -100,26 +107,22 @@ async def help(ctx, command: typing.Optional[str] = "default_help"):
     if command == "default_help":
         embed = discord.Embed(title = "Help", description = "My prefix is `{prefix}`, get more informations on specific commands with `{prefix}help <command>`.".format(prefix = bot_prefix))
         embed.add_field(name = "Commands", value = "* `collections`\n* `info`\n* `picture`")
-        embed.set_footer(text = "Requested by {message_author}".format(message_author = ctx.message.author))
         await ctx.send(embed = embed)
 
     elif command == "collections":
         embed = discord.Embed(title = "Help (collections)", description = "Sends a message containing a list of available collections.")
         embed.add_field(name = "Example", value = "`{prefix}collections`".format(prefix = bot_prefix))
-        embed.set_footer(text = "Requested by {message_author}".format(message_author = ctx.message.author))
         await ctx.send(embed = embed)
 
     elif command == "info":
         embed = discord.Embed(title = "Help (info)", description = "Sends a message containing relevant information to the bot.")
         embed.add_field(name = "Example", value = "`{prefix}info`".format(prefix = bot_prefix))
-        embed.set_footer(text = "Requested by {message_author}".format(message_author = ctx.message.author))
         await ctx.send(embed = embed)
 
     elif command == "picture":
         embed = discord.Embed(title = "Help (picture)", description = "Sends a random image from the specified collection.")
         embed.add_field(name = "Example", value = "`{prefix}picture <collection>`".format(prefix = bot_prefix), inline = True)
         embed.add_field(name = "Example 2", value = "`pretty <collection>`".format(prefix = bot_prefix), inline = True)
-        embed.set_footer(text = "Requested by {message_author}".format(message_author = ctx.message.author))
         await ctx.send(embed = embed)
 
 @bot.command()
@@ -133,7 +136,6 @@ async def collections(ctx):
     del collections_list[-1]
 
     embed = discord.Embed(title = "Collections", description = "Collections can be specified with or without the 's' at the end.\n{list}".format(list = "\n * ".join(collections_list)))
-    embed.set_footer(text = "Requested by {message_author}".format(message_author = ctx.message.author))
     await ctx.send(embed = embed)
 
 @bot.command()
@@ -146,11 +148,14 @@ async def picture(ctx, collection:str):
 
     jpegs = list(collection_dir.glob("**/*.jpg"))
     choice = random.choice(jpegs)
+    author = bot.get_user(choice.parent.name)
+    if author is None:
+        author = await bot.fetch_user(choice.parent.name)
 
     embed = discord.Embed(title = "{collection} picture".format(collection = collection_lowercase))
     image = discord.File(choice, filename = choice.name)
     embed.set_image(url = "attachment://{file}".format(file = choice.name))
-    embed.set_footer(text = "Requested by {message_author}".format(message_author = ctx.message.author))
+    embed.set_footer(text = "Image submitted by {author}".format(author = author))
     await ctx.send(file = image, embed = embed)
 
 @bot.command()
@@ -158,7 +163,6 @@ async def info(ctx, description = "Get information about the bot"):
     embed = discord.Embed(title = "Information", description = "I'm a bot made by Flipp3rrr#6969. I got various picture related commands and some other fun commands! Find out more about my commands with `{prefix}help`".format(prefix = bot_prefix))
     embed.add_field(name = "Invite", value = bot_invite, inline = True)
     embed.add_field(name = "GitHub", value = github_link, inline = True)
-    embed.set_footer(text = "Requested by {message_author}".format(message_author = ctx.message.author))
     await ctx.send(embed = embed)
 
 bot.run(token)
